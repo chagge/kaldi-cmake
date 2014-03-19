5f1b7905083c0f900226b4da0651f05b1b167724
hohlov-yuri <hohlov-yuri@yandex.ru>
2014-03-19 20:53:32 +0400

    +

--- kaldi-cmake.py
+++ kaldi-cmake.py
@@ -8,6 +8,8 @@ import re
 #import random
 from argparse import ArgumentParser
 
+# --kaldi=..\kaldi-trunk --project=..\trunk
+
 parser = ArgumentParser(prog = "kaldi-cmake", description = "Скрипт для создания файлов проекта под CMake.")
 parser.add_argument ('-k', '--kaldi', default = os.getenv("KALDI_ROOT"), help = "Путь к директории с исходниками Kaldi.", metavar = 'DIR')
 parser.add_argument ('-p', '--project', default = str(os.path.join(os.path.abspath(os.curdir), "project")), help = 'Путь к директории, в которой нужно создать проект.', metavar = 'DIR')
@@ -18,10 +20,14 @@ if namespace.kaldi == None:
 print("Параметры запуска: {}\n".format(namespace))
 
 g_trunk_src = str(os.path.join(os.path.abspath(namespace.kaldi), "src")).replace('\\', '/')
-g_proj_src = os.path.join(namespace.project, "kaldi")
+g_proj_src = os.path.abspath(namespace.project)
 
 g_cmake_src = "cmake"
 g_cmake_dst = os.path.join(g_proj_src, "cmake")
+g_externals_src = "externals"
+g_externals_dst = os.path.join(g_proj_src, "externals")
+g_patched_src = "patched"
+g_patched_dst = g_proj_src
 g_cmake_hdr = "CMakeLists.txt"
 
 g_inc_name = "include"
@@ -29,7 +35,8 @@ g_src_name = "src"
 g_tst_name = "test"
 
 #g_allowed = ["base", "cudamatrix", "feat", "lm", "matrix", "nnet", "tree", "util"]
-g_allowed = ["base", "cudamatrix", "feat", "matrix", "util"]
+g_allowed_bin = ["bin"]
+g_allowed_lib = ["base", "cudamatrix", "feat", "gmm", "hmm", "matrix", "thread", "tree", "util"]
 g_excl_dir = ["doc", "gst-plugin", "makefiles", "online", "onlinebin"]
 g_excl_src = [
     "cu-device.cc",
@@ -43,7 +50,7 @@ g_excl_src = [
     "kaldi-table-test.cc",
     "timer.h"
 ]
-g_external = ["fst", "irstlm"]
+g_external = {"fst":"fst", "irstlm":None}
 
 g_inc_dir = os.path.join(g_proj_src, g_inc_name)
 g_src_dir = os.path.join(g_proj_src, g_src_name)
@@ -111,6 +118,15 @@ class KaldiModule:
                 _lines.append("\t" + source.ref_path)
             _lines.append(")")
 
+def CopyTree(_dir_src, _dir_dst):
+    for root, dirs, files in os.walk(_dir_src):
+        for name in files:
+            path_src = os.path.join(root, name)
+            path_dst = str(path_src).replace(_dir_src, _dir_dst)
+            if os.path.exists(path_dst):
+                os.remove(path_dst)
+            shutil.copyfile(path_src, path_dst)
+
 def IsApplication(_path):
     lines = []
     with open(_path, "r", encoding="utf-8") as file_src:
@@ -124,7 +140,7 @@ def IsApplication(_path):
 
 def IsAllowed(_module):
     for depend in module.depends:
-        if not (depend in g_allowed):
+        if not (depend in g_allowed_bin) and not (depend in g_allowed_lib):
             return False
     for src_file in _module.src_lst:
         if os.path.basename(src_file.ref_path) in g_excl_src:
@@ -153,7 +169,12 @@ def AddExecutable(_file, _module, _headers, _regist):
     _file.write("\nadd_executable(" + mod_name + " " + _module.src_lst[0].ref_path + ")\n")
     _file.write("set_default_executable_target_properties(" + mod_name + ")\n")
 #    _file.write("import_static_library(" + mod_name + " " + _module.name + ")\n")
-    for depend in g_allowed:
+
+    for depend in g_external:
+        if g_external[depend] != None:
+            _file.write("import_shared_library(" + mod_name + " " + depend + ")\n")
+
+    for depend in g_allowed_lib:
         if not (depend in _headers):
             _file.write("import_static_library(" + mod_name + " " + depend + ")\n")
     _file.write("if (UNIX)\n")
@@ -164,11 +185,13 @@ def AddExecutable(_file, _module, _headers, _regist):
 
 print("======= BEGIN =======")
 
-if os.path.exists(g_proj_src):
-    print("Удаление директории: " + g_proj_src)
-    shutil.rmtree(g_proj_src, True)
+#if os.path.exists(g_proj_src):
+#    print("Удаление директории: " + g_proj_src)
+#    shutil.rmtree(g_proj_src, True)
 print("Копирование директории: " + g_cmake_src)
 shutil.copytree(g_cmake_src, g_cmake_dst)
+print("Копирование директории: " + g_externals_src)
+shutil.copytree(g_externals_src, g_externals_dst)
 print("Создание директории: " + g_inc_dir)
 os.mkdir(g_inc_dir)
 print("Создание директории: " + g_src_dir)
@@ -237,14 +260,14 @@ hdr_only = []
 for module in lib_modules:
     if len(module.src_lst) == 0:
         hdr_only.append(module.name)
-        g_allowed.append(module.name)
+        g_allowed_lib.append(module.name)
 for modules in [lib_modules, exe_modules, tst_modules]:
     for module in modules:
         for mod_name in hdr_only:
             module.depends.discard(mod_name)
 modules = []
 for module in lib_modules:
-    if module.name in g_allowed:
+    if module.name in g_allowed_lib:
         modules.append(module)
 lib_modules = modules
 
@@ -278,276 +301,12 @@ with open(g_lst_cmake, "w", encoding="utf-8") as file_txt:
     for module in exe_modules:
         if IsAllowed(module):
             AddExecutable(file_txt, module, hdr_only, regist)
-    for module in tst_modules:
-        if IsAllowed(module):
-            AddExecutable(file_txt, module, hdr_only, regist)
+    #for module in tst_modules:
+    #    if IsAllowed(module):
+    #        AddExecutable(file_txt, module, hdr_only, regist)
     file_txt.close()
 
-print("======= END =======")
+#print("Копирование директории: " + g_patched_src)
+#CopyTree(g_patched_src, g_patched_dst)
 
-#exit()
-#
-#g_word_sil = "<SIL>"
-#g_phone_sil = "SIL"
-#g_utils_dir = "utils"
-#
-#class StrList:
-#    lines = None
-#
-#    def __init__(self):
-#        self.lines = []
-#    
-#    def Clear(self):
-#        self.lines = []
-#        
-#    def Append(self, _line):
-#        self.lines.append(_line)
-#        
-#    def Save(self, _path):
-#        self.lines.sort()
-#        with open(_path, "w", encoding="utf-8") as file_txt:
-#            for line in self.lines:
-#                file_txt.write(line + "\n")
-#            file_txt.close()
-#
-#class WordInfo:
-#    text = None
-#    tran = None
-#    word = None
-#    
-#    def __init__(self, _text, _tran, _word):
-#        self.text = _text.lower()
-#        self.tran = _tran
-#        self.word = int(_word)
-#
-#def SortWordsById(_word):
-#    return _word.word
-#
-#class Vocabulary:
-#    words = None
-#    
-#    def __init__(self, _lines):
-#        self.words = []
-#        for line in _lines:
-#            match = re.search("[Common]\\s*$", line)
-#            if match:
-#                break
-#            match = re.search("^(.+)=(.+)#(\\d+)\\s*$", line)
-#            if match:
-#                self.words.append(WordInfo(match.groups()[0], match.groups()[1], match.groups()[2]))
-#        self.words.sort(key=SortWordsById)
-#        
-#    def Text(self):
-#        text = None
-#        word_id = 0
-#        for word in self.words:
-#            if word.word == word_id:
-#                if text == None:
-#                    text = word.text
-#                else:
-#                    text = text + " " + word.text
-#                word_id = word_id + 1
-#        return text
-#
-#class FileInfo:
-#    root = None
-#    name = None
-#    path = None
-#    spkr = None
-#    sent = None
-#    vocb = None
-#    text = None
-#    
-#    def __init__(self, _root, _name, _path, _spkr, _sent, _vocb):
-#        self.root = _root
-#        self.name = _name
-#        self.path = _path
-#        self.spkr = _spkr
-#        self.sent = _sent
-#        self.vocb = _vocb
-#        self.text = _vocb.Text()
-#
-#class FileList(list):
-#    def __init__(self):
-#        list.__init__(self, [])
-#        
-#    def Load(self, _path_sdv, _path_wav):
-#        list.__init__(self, [])
-#        for root, dirs, files in os.walk(_path_sdv):
-#            for name in files:
-#                match = re.search("(\\d{3})_(\\d{5})\\.sdv$", name.lower())
-#                if not match:
-#                    continue
-#                path_wav = os.path.join(_path_wav, os.path.basename(root), os.path.splitext(name)[0] + ".wav")
-#                if not os.path.exists(path_wav):
-#                    continue
-#                path_sdv = os.path.join(root, name)
-#                lines = []
-#                with open(path_sdv, "r", encoding="CP1251") as file_sdv:
-#                    lines = file_sdv.readlines()
-#                    file_sdv.close()
-#                spkr_id = match.groups()[0]
-#                sent_id = spkr_id + "_" + match.groups()[1]
-#                self.append(FileInfo(root, name, path_wav, spkr_id, sent_id, Vocabulary(lines)))
-#
-#def CreateWavList(_path, _info_lst):
-#    lines = StrList()
-#    for info in _info_lst:
-#        lines.Append(info.path)
-#    lines.Save(_path)
-#
-#def CreateWavScp(_path, _info_lst):
-#    lines = StrList()
-#    for info in _info_lst:
-#        lines.Append(info.sent + " " + info.path)
-#    lines.Save(_path)
-#
-#def CreateWavTxt(_path, _info_lst):
-#    lines = StrList()
-#    for info in _info_lst:
-#        lines.Append(info.sent + " " + info.text)
-#    lines.Save(_path)
-#
-#def CreateUtt2Spk(_path, _info_lst):
-#    lines = StrList()
-#    for info in _info_lst:
-#        lines.Append(info.sent + " " + info.spkr)
-#    lines.Save(_path)
-#
-#def CreateLexicon(_path_lexicon, _path_lex_words, _info_lst):
-#    lexicon = set([])
-#    for info in _info_lst:
-#        for word in info.vocb.words:
-#            lexicon.add(word.text + " " + word.tran)
-#    lines = StrList()
-#    for line in lexicon:
-#        lines.Append(line)
-#    lines.Save(_path_lex_words)
-#    lines.Append(g_word_sil + " " + g_phone_sil)
-#    lines.Save(_path_lexicon)
-#
-#def CreatePhoneTable(_path, _info_lst):
-#    phset = set([])
-#    for info in _info_lst:
-#        for word in info.vocb.words:
-#            phones = word.tran.split(" ")
-#            for phone in phones:
-#                phset.add(phone)
-#    phones = StrList()
-#    for phone in phset:
-#        phones.Append(phone)
-#    phones.Save(_path)
-#
-#def CreateSilTables(_path_sil, _path_opt_sil):
-#    silences = StrList()
-#    silences.Append(g_phone_sil)
-#    silences.Save(_path_sil)
-#    silences.Save(_path_opt_sil)
-#
-#def CreateLangModel(_path, _info_lst):
-#    wdset = set([])
-#    for info in _info_lst:
-#        for word in info.vocb.words:
-#            wdset.add(word.text)
-#    words = list(wdset)
-#    words.sort()
-#    with open(_path, "w", encoding="utf-8") as file_txt:
-#        file_txt.write("\\data\\\n")
-#        file_txt.write("ngram 1=" + str(len(words) + 2) + "\n")
-#        file_txt.write("\\1-grams:\n")
-#        file_txt.write("-99\t<s>\n")
-#        file_txt.write("-1\t</s>\n")
-#        for word in words:
-#            file_txt.write("-1\t" + word + "\n")
-#        file_txt.write("\\end\\\n")
-#        file_txt.close()
-#
-#print("======= BEGIN =======")
-#
-#print("Рабочая директория: " + os.path.abspath(os.curdir))
-#path_tmp = os.path.join(os.path.abspath(os.curdir), "wav")
-#if not os.path.exists(path_tmp):
-#    print("Директория не существует: " + path_tmp)
-#    exit()
-#path_tmp = "exp"
-#if os.path.exists(path_tmp):
-#    print("Удаление директории: " + path_tmp)
-#    shutil.rmtree(path_tmp, True)
-#path_tmp = "mfcc"
-#if os.path.exists(path_tmp):
-#    print("Удаление директории: " + path_tmp)
-#    shutil.rmtree(path_tmp, True)
-#g_data_dir = "data"
-#if "data_dir" in os.environ:
-#    g_data_dir = os.environ["data_dir"]
-#if os.environ.has_key("data_dir"):
-#    g_data_dir = os.environ["data_dir"]
-#if os.path.exists(g_data_dir):
-#    print("Удаление директории: " + g_data_dir)
-#    shutil.rmtree(g_data_dir, True)
-#print("Создание директории: " + g_data_dir)
-#os.mkdir(g_data_dir)
-#g_local_dir = os.path.join(g_data_dir, "local")
-#print("Создание директории: " + g_local_dir)
-#os.mkdir(g_local_dir)
-#g_dict_dir = os.path.join(g_local_dir, "dict")
-#print("Создание директории: " + g_dict_dir)
-#os.mkdir(g_dict_dir)
-#
-#print("Загрузка sdv-файлов (train)...")
-#g_train_lst = FileList()
-#g_train_lst.Load("sent", os.path.join("wav", "train"))
-#print(" Обучающая выборка: " + str(len(g_train_lst)) + " файлов.")
-#print("Загрузка sdv-файлов (test)...")
-#g_test_lst = FileList()
-#g_test_lst.Load("sent", os.path.join("wav", "test"))
-#print(" Тестовая выборка: " + str(len(g_test_lst)) + " файлов.")
-#g_file_lst = g_train_lst + g_test_lst
-#print("Всего загружено " + str(len(g_file_lst)) + " sdv-файлов.")
-#
-#print("Создание файла waves_all.list...")
-#CreateWavList(os.path.join(g_local_dir, "waves_all.list"), g_file_lst)
-#print("Создание файла waves.test...")
-#CreateWavList(os.path.join(g_local_dir, "waves.test"), g_test_lst)
-#print("Создание файла waves.train...")
-#CreateWavList(os.path.join(g_local_dir, "waves.train"), g_train_lst)
-#
-#print("Создание файла test_wav.scp...")
-#CreateWavScp(os.path.join(g_local_dir, "test_wav.scp"), g_test_lst)
-#print("Создание файла train_wav.scp...")
-#CreateWavScp(os.path.join(g_local_dir, "train_wav.scp"), g_train_lst)
-#
-#print("Создание файла test.txt...")
-#CreateWavTxt(os.path.join(g_local_dir, "test.txt"), g_test_lst)
-#print("Создание файла train.txt...")
-#CreateWavTxt(os.path.join(g_local_dir, "train.txt"), g_train_lst)
-#
-#for data_set in [["test", g_test_lst], ["train", g_train_lst]]:
-#    dir_dst = os.path.join(g_data_dir, data_set[0])
-#    print("Создание директории: " + dir_dst)
-#    os.mkdir(dir_dst)
-#    print("Копирование файлов text и wav.scp...")
-#    shutil.copyfile(os.path.join(g_local_dir, data_set[0] + ".txt"), os.path.join(dir_dst, "text"))
-#    shutil.copyfile(os.path.join(g_local_dir, data_set[0] + "_wav.scp"), os.path.join(dir_dst, "wav.scp"))
-#    print("Создание файла utt2spk...")
-#    path_src = os.path.join(dir_dst, "utt2spk")
-#    CreateUtt2Spk(path_src, data_set[1])
-#    print("Создание файла spk2utt...")
-#    path_dst = os.path.join(dir_dst, "spk2utt")
-#    cmd_line = os.path.join(g_utils_dir, "utt2spk_to_spk2utt.pl") + " < " + path_src + " > " + path_dst
-#    proc = os.popen(cmd_line)
-#    for line in proc.readlines():
-#        print(line)
-#
-#print("Создание файлов task.arpa и lm_test.arpa...")
-#CreateLangModel(os.path.join(g_local_dir, "task.arpa"), g_test_lst)
-#shutil.copyfile(os.path.join(g_local_dir, "task.arpa"), os.path.join(g_local_dir, "lm_test.arpa"))
-#
-#print("Создание файлов lexicon.txt и lexicon_words.txt...")
-#CreateLexicon(os.path.join(g_dict_dir, "lexicon.txt"), os.path.join(g_dict_dir, "lexicon_words.txt"), g_file_lst)
-#print("Создание файла nonsilence_phones.txt...")
-#CreatePhoneTable(os.path.join(g_dict_dir, "nonsilence_phones.txt"), g_file_lst)
-#print("Создание файлов silence_phones.txt и optional_silence.txt...")
-#CreateSilTables(os.path.join(g_dict_dir, "silence_phones.txt"), os.path.join(g_dict_dir, "optional_silence.txt"))
-#
-#print("======= END =======")
+print("======= END =======")
